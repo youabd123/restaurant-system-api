88eb85bf17115d01eb550dd5cef831986eec3916
yonisb77 <yonis_77@hotmail.com>
2026-05-13 15:43:41 +0200

    Added seed data for Categories and MenuItems

---
+++ RestaurantSystem.Infrastructure/Data/Migrations/20260513131534_SeedInitialData.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace RestaurantSystem.Infrastructure.Data.Migrations
+{
+   
+    public partial class SeedInitialData : Migration
+    {
+      
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "Description", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Stenugnsbakade pizzor", "Pizzor" },
+                    { 2, "Kalla drycker", "Dryck" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "MenuItems",
+                columns: new[] { "Id", "CategoryId", "Description", "IsAvailable", "Name", "Price" },
+                values: new object[,]
+                {
+                    { 1, 1, "Tomatsås, ost och basilika", true, "Margherita", 95.00m },
+                    { 2, 2, "33cl", true, "Coca Cola", 25.00m }
+                });
+        }
+
+     
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "MenuItems",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "MenuItems",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
