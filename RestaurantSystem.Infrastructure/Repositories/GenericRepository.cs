56f3e71bc4373342c6beec13c1fba746de030fca
youabd123 <yousuf.hh@icloud.com>
2026-05-24 10:30:33 +0200

    Load order details with menu items

--- RestaurantSystem.Infrastructure/Repositories/GenericRepository.cs
+++ RestaurantSystem.Infrastructure/Repositories/GenericRepository.cs
@@ -15,9 +15,10 @@ public class GenericRepository<T> : IGenericRepository<T> where T : class
         _dbSet = context.Set<T>();
     }
 
-    public async Task<List<T>> GetAllAsync() => await _dbSet.ToListAsync();
+    public virtual async Task<List<T>> GetAllAsync() => await _dbSet.ToListAsync();
 
-    public async Task<T?> GetByIdAsync(int id) => await _dbSet.FindAsync(id);
+    public virtual async Task<T?> GetByIdAsync(int id) => await _dbSet.FindAsync(id);
+
 
     public async Task<T> CreateAsync(T entity)
     {
--- RestaurantSystem.Infrastructure/Repositories/OrderRepository.cs
+++ RestaurantSystem.Infrastructure/Repositories/OrderRepository.cs
@@ -1,4 +1,5 @@
-﻿using RestaurantSystem.Application.Common.Interfaces;
+﻿using Microsoft.EntityFrameworkCore;
+using RestaurantSystem.Application.Common.Interfaces;
 using RestaurantSystem.Domain.Entities;
 using RestaurantSystem.Infrastructure.Data;
 
@@ -8,5 +9,23 @@ public class OrderRepository : GenericRepository<Order>, IOrderRepository
 {
     public OrderRepository(AppDbContext context) : base(context)
     {
+    }
+
+    public override async Task<List<Order>> GetAllAsync()
+
+    {
+        return await _context.Orders
+            .Include(order => order.OrderItems)
+                .ThenInclude(orderItem => orderItem.MenuItem)
+            .ToListAsync();
+    }
+
+    public override async Task<Order?> GetByIdAsync(int id)
+
+    {
+        return await _context.Orders
+            .Include(order => order.OrderItems)
+                .ThenInclude(orderItem => orderItem.MenuItem)
+            .FirstOrDefaultAsync(order => order.Id == id);
     }
 }
